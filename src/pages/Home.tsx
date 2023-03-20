51be17cb580863637a1728cd04cbfc03424371ab
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-20 09:21:12 +0700

    feat: add homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = (props: { name: string }) => {
+  return <div>{props.name ? "Hi " + props.name : "You are not logged in"}</div>;
+};
+
+export default Home;
