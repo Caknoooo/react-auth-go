66acf825b18a99b237269d5e0b1910fae3eb433e
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-20 09:22:19 +0700

    feat: add simple login section

---
+++ src/pages/Login.tsx
@@ -0,0 +1,60 @@
+import React, { SyntheticEvent, useState } from "react";
+import { redirect, useNavigate } from "react-router-dom";
+
+const Login = (props: { setName: (name: string) => void }) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8888/api/user/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email, 
+            password
+          }),
+        }
+      );
+      await response.json();
+      alert('Berhasil Login');
+    } catch (error) {
+      alert(error);
+    } 
+    navigate('/');
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <input
+        type="email"
+        className="form-control"
+        placeholder="Email address"
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button className="w-100 btn btn-lg btn-primary" type="submit">
+        Sign in
+      </button>
+    </form>
+  );
+};
+
+export default Login;
